662e5ed36a89c7bec131545884596b87a43fe3da
Andrew Chen <andrewtheannihilator@gmail.com>
2012-12-03 17:47:01 -0800

    new windowsessions

--- scripts/windowSession1.sh
+++ scripts/windowSession1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #urxvt -cd ~/wingMorphing_workspace/wingMorphing.gl -e sh -c '~/bin/ranger-1.5.4/ranger.py'
-urxvt -cd ~/diatimic_workspace/diatimic -e sh -c '~/bin/ranger-1.5.4/ranger.py'
+urxvt -cd ~/grizzly_workspace/grizzly -e sh -c '~/bin/ranger-1.5.4/ranger.py'
